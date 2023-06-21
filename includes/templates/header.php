5accc98e31faab72da863b2d41ddf85e024ae978
Hemsama <54726855+Hemsama@users.noreply.github.com>
2023-06-20 19:58:55 -0500

    Add files via upload

---
+++ includes/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RCA</title>
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="icon" href="build/img/favi-icon.webp">
+</head>
+<body>
+    
+    <header class="header">
+        <div class="contenedor">
+            <div class="barra">
+                <a href="index.php">
+                    <img src="build/img/logo.webp" alt="Logotipo de RCA">
+                </a>
+
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+
+                <nav class="navegacion">
+                    <a href="ingreso.php">Ingreso</a>
+                    <a href="salida.php">Salida</a>
+                    <a href="registros.php">Registros</a>
+                </nav>
+
+            </div>
+        </div> <!--.barra-->
+    </header>
